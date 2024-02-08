31972551a3e147df257b4930d1d935e84f26fbf0
Theo <theo.denanassy@epitech.eu>
2024-02-08 15:30:33 +0100

    psuh assets part 2

--- Source/Runner/Private/Charaters/RunCharacter.cpp
+++ Source/Runner/Private/Charaters/RunCharacter.cpp
@@ -1,6 +1,3 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "Charaters/RunCharacter.h"
 #include "GameFramework/SpringArmComponent.h"
 #include "../../Public/GameMode/GameModeRunner.h"
